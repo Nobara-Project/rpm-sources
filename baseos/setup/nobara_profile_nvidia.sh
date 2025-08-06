3c25bb50a63b4111531d0bd1e108e46c78ed3546
GloriousEggroll <gloriouseggroll@gmail.com>
2025-08-06 07:36:20 -0600

    bump inputplumber

--- baseos/setup/nobara_profile_nvidia.sh
+++ baseos/setup/nobara_profile_nvidia.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/sh
-# https://bugs.kde.org/show_bug.cgi?id=495260
-export KWIN_DRM_DISABLE_TRIPLE_BUFFERING=0
 
 # nvidia rendering fixup
 if [ -f /bin/lspci ]; then
@@ -9,8 +7,9 @@ if [ -f /bin/lspci ]; then
     export LIBVA_DRIVER_NAME=nvidia
     export MOZ_DISABLE_RDD_SANDBOX=1
     export EGL_PLATFORM="$XDG_SESSION_TYPE"
-    # https://bugs.kde.org/show_bug.cgi?id=488941,
-    export KWIN_DRM_ALLOW_NVIDIA_COLORSPACE=1
     export GAMESCOPE_WSI_HIDE_PRESENT_WAIT_EXT=1
+    export __EGL_VENDOR_LIBRARY_FILENAMES=/usr/share/glvnd/egl_vendor.d/10_nvidia.json
+    export __NV_PRIME_RENDER_OFFLOAD=1
+    export __GLX_VENDOR_LIBRARY_NAME=nvidia
   fi
 fi
