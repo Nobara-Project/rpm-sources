b0b4bec8b51cf63098c9fac91f515d8c53720588
GloriousEggroll <gloriouseggroll@gmail.com>
2026-03-18 18:17:44 -0600

    fixup

--- baseos/yumex/yumex-ng-4946515e509b12592317ad2668bd824a6f81f054/tests/test_util.py
+++
@@ -1,6 +0,0 @@
-from yumex.utils import get_distro_release
-
-
-def test_get_distro_release():
-    release = get_distro_release()
-    assert release == "43"
--- baseos/yumex/yumex-ng-4946515e509b12592317ad2668bd824a6f81f054/yumex/utils/exceptions.py
+++
@@ -1,4 +0,0 @@
-class YumexException(Exception):
-    def __init__(self, msg: str, *args):
-        super().__init__(*args)
-        self.msg = msg
